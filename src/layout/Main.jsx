fe79126472960390195c2e702f08f83c4c156321
Olha Maievska <olhamaievska91@gmail.com>
2022-08-31 12:53:00 +0200

    changed request

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -15,23 +15,30 @@ class Main extends React.Component {
   }
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((res) => res.json())
       .then((data) => {
         this.setState({ cards: data.Search, loading: false })
       })
-      .catch((e) => console.error(e.message))
+      .catch((e) => {
+        console.error(e)
+        this.setState({ loading: false })
+      })
   }
 
   searchMovies = (str, type = 'all') => {
     this.setState({ loading: true })
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }`
     )
       .then((res) => res.json())
       .then((data) => this.setState({ cards: data.Search, loading: false }))
+      .catch((e) => {
+        console.error(e)
+        this.setState({ loading: false })
+      })
   }
 
   render() {
